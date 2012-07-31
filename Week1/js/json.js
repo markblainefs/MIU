703b9406ebd1ef524b3382a8902066302535c8c0
Mark Blaine <markblaine@fullsail.edu>
2012-07-30 20:41:46 -0500

    First commit
    New folders and files with 20 item JSON

---
+++ Week1/js/json.js
@@ -0,0 +1,182 @@
+var json = {
+	"todo1":	{
+		"taskName":		["Task name:", "Test one"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo2":	{
+		"taskName":		["Task name:", "Test two"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo3":	{
+		"taskName":		["Task name:", "Test three"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo4":	{
+		"taskName":		["Task name:", "Test four"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo5":	{
+		"taskName":		["Task name:", "Test five"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo6":	{
+		"taskName":		["Task name:", "Test six"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo7":	{
+		"taskName":		["Task name:", "Test seven"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo8":	{
+		"taskName":		["Task name:", "Test eight"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo9":	{
+		"taskName":		["Task name:", "Test nine"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo10":	{
+		"taskName":		["Task name:", "Test ten"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo11":	{
+		"taskName":		["Task name:", "Test eleven"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo12":	{
+		"taskName":		["Task name:", "Test twelve"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo13":	{
+		"taskName":		["Task name:", "Test thirteen"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo14":	{
+		"taskName":		["Task name:", "Test fourteen"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo15":	{
+		"taskName":		["Task name:", "Test fifteen"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo16":	{
+		"taskName":		["Task name:", "Test sixteen"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo17":	{
+		"taskName":		["Task name:", "Test seventeen"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	},
+	"todo18":	{
+		"taskName":		["Task name:", "Test eighteen"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some more test data"]
+	},
+	"todo19":	{
+		"taskName":		["Task name:", "Test nineteen"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some test data"]
+	},
+	"todo20":	{
+		"taskName":		["Task name:", "Test twenty"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "This is some other test data"]
+	}
+}
