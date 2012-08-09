37ad4b73a3e09d760b6a08d25266a8c98f2da9d6
Mark Blaine <markblaine@fullsail.edu>
2012-08-09 12:27:46 -0500

    Setting up for jquery

---
+++ Week2/Bronze/js/json.js
@@ -0,0 +1,182 @@
+var json = {
+	"todo1":	{
+		"taskName":		["Task name:", "Paint kitchen"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Paint the kitchen walls blue"]
+	},
+	"todo2":	{
+		"taskName":		["Task name:", "Get raise"],
+		"group":		["Location:", "Office"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Ask for a raise since you have more responsibilty"]
+	},
+	"todo3":	{
+		"taskName":		["Task name:", "Change oil"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Use Pennzoil"]
+	},
+	"todo4":	{
+		"taskName":		["Task name:", "Groceries"],
+		"group":		["Location:", "Shop"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Get lemons and milk"]
+	},
+	"todo5":	{
+		"taskName":		["Task name:", "Mail taxes"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Pick up returns from CPA and take to Post Office"]
+	},
+	"todo6":	{
+		"taskName":		["Task name:", "Fix rack"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Straighten the bent shelf"]
+	},
+	"todo7":	{
+		"taskName":		["Task name:", "Get credit"],
+		"group":		["Location:", "Office"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Call Verizon about charges"]
+	},
+	"todo8":	{
+		"taskName":		["Task name:", "Wax"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Use Blue Coral"]
+	},
+	"todo9":	{
+		"taskName":		["Task name:", "Roses"],
+		"group":		["Location:", "Shop"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Buy roses for anniversary"]
+	},
+	"todo10":	{
+		"taskName":		["Task name:", "Donuts"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Drop donuts at nursing home"]
+	},
+	"todo11":	{
+		"taskName":		["Task name:", "Treehouse"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Build treehouse in yard"]
+	},
+	"todo12":	{
+		"taskName":		["Task name:", "Eval"],
+		"group":		["Location:", "Office"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Give employee her eval"]
+	},
+	"todo13":	{
+		"taskName":		["Task name:", "Wipers"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Replace wipers with RainX 20 - 22"]
+	},
+	"todo14":	{
+		"taskName":		["Task name:", "Perfume"],
+		"group":		["Location:", "Shop"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Coco Chanel"]
+	},
+	"todo15":	{
+		"taskName":		["Task name:", "Lunch w MIL"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Take MIL to Petroleum Club"]
+	},
+	"todo16":	{
+		"taskName":		["Task name:", "Faucet"],
+		"group":		["Location:", "Home"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Replace dripping faucet"]
+	},
+	"todo17":	{
+		"taskName":		["Task name:", "Breakfast"],
+		"group":		["Location:", "Office"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Set up catering for staff meeting"]
+	},
+	"todo18":	{
+		"taskName":		["Task name:", "Febreeze"],
+		"group":		["Location:", "Car"],
+		"dueDate":		["Due date:", "2012-07-09"],
+		"importance":	["Importance:", "8"],
+		"priority":		["Because:", "I'm in trouble"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Get moldy smell out of trunk"]
+	},
+	"todo19":	{
+		"taskName":		["Task name:", "Socks"],
+		"group":		["Location:", "Shop"],
+		"dueDate":		["Due date:", "2012-07-07"],
+		"importance":	["Importance:", "6"],
+		"priority":		["Because:", "I need to"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Buy some Gold Toes"]
+	},
+	"todo20":	{
+		"taskName":		["Task name:", "Museum"],
+		"group":		["Location:", "Errand"],
+		"dueDate":		["Due date:", "2012-07-08"],
+		"importance":	["Importance:", "7"],
+		"priority":		["Because:", "I'm awesome"],
+		"honey":		["List for:", "Me"],
+		"specialInstructions":	["Details:", "Take nephew to MOMA"]
+	}
+}
