efc7c1b9c2577061d5d16943dc4e05fd7ca39ad1
Mark Blaine <markblaine@fullsail.edu>
2012-08-23 22:18:00 -0500

    Added branding and made bronze app show bad examples of deep background and few hues many values

--- Week4/Gold/js/main.js
+++ Week4/Gold/js/main.js
@@ -2,7 +2,6 @@ $('#main').on('pageinit', function(){
 	//code needed for main page goes here
 });	
 		
-/*
 $('#addItem').on('pageinit', function(){
 		delete $.validator.methods.date;
 		var myForm = $('#addItemForm');
@@ -19,7 +18,6 @@ $('#addItem').on('pageinit', function(){
 	
 });
 
-*/
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 var autofillData = function (){
